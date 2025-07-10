9fcb40f42a5e2610a3fcf8efa07e1e2f88cdae67
MueblesFusion <mueblesfusion855@gmail.com>
2025-07-10 15:44:30 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -181,7 +181,7 @@ export default function HomePage() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center space-x-4">
-             <Image src="/logo.png"alt="Muebles Fusión "width={200}height={60}priority/>
+          
 
             </div>
             <div className="flex space-x-4">
