7d221a325325dcd500e2e2729e0ecb4e0f5eed16
MueblesFusion <mueblesfusion855@gmail.com>
2025-07-11 12:47:29 -0600

    Update page.tsx

--- app/planes/page.tsx
+++ app/planes/page.tsx
@@ -1,6 +1,7 @@
+"use client"
 import ResponsiveWrapper from "@/components/ResponsiveWrapper"
 
-"use client"
+
 
 import { useEffect, useState } from "react"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
