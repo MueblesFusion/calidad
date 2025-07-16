71c2ebf01f36eef9834dba56d47be220f4f3d8a2
MueblesFusion <mueblesfusion855@gmail.com>
2025-07-16 13:53:53 -0600

    Update page.tsx

--- app/planes/page.tsx
+++ app/planes/page.tsx
@@ -14,7 +14,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/u
 import { useToast } from "@/hooks/use-toast"
 import { createClient } from "@supabase/supabase-js"
 import { Loader2 } from "lucide-react"
-import * as XLSX from "xlsx"
+import * as XLSX from "xlsx-style"
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
